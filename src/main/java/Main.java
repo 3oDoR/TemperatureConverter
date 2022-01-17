cc4ac60d744e8561f141822e8c740b1a819d60ae
Nikita <smirnov2.nv@gmail.com>
2022-01-17 23:24:35 +0300

    Replaced void method with double for tests and

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,82 +2,100 @@ import java.util.Scanner;
 
 public class Main {
 
-    public void convertCtoF(String str, Scanner in) {
+    public double convertCtoF(String str, Scanner in) {
         if (str.equalsIgnoreCase("c f")) {
             System.out.println("Enter temperature(C)");
             str = in.nextLine();
             try {
                 double sum = Double.parseDouble(str) * 9 / 5 + 32;
                 System.out.println(str + "C = " + sum + "F");
+                return sum;
             } catch (Exception e) {
                 System.out.println("Number input error. Next time, please try to enter a number. Example: 23 or 23.55");
+                return 0;
             }
         }
+        return 0;
     }
 
-    public void convertCtoK(String str, Scanner in) {
+    public double convertCtoK(String str, Scanner in) {
         if (str.equalsIgnoreCase("c k")) {
             System.out.println("Enter temperature(C)");
             str = in.nextLine();
             try {
                 double sum = (Double.parseDouble(str) + 273.15D);
                 System.out.println(str + "C = " + sum + "K");
+                return sum;
             } catch (Exception e) {
                 System.out.println("Number input error. Next time, please try to enter a number. Example: 23 or 23.55");
+                return 0;
             }
         }
+        return 0;
     }
 
-    public void convertFtoC(String str, Scanner in) {
+    public double convertFtoC(String str, Scanner in) {
         if (str.equalsIgnoreCase("f c")) {
             System.out.println("Enter temperature(F)");
             str = in.nextLine();
             try {
                 double sum = (Double.parseDouble(str) - 32) * 5 / 9;
                 System.out.println(str + "F = " + sum + "C");
+                return sum;
             } catch (Exception e) {
                 System.out.println("Number input error. Next time, please try to enter a number. Example: 23 or 23.55");
+                return 0;
             }
         }
+        return 0;
     }
 
-    public void convertFtoK(String str, Scanner in) {
+    public double convertFtoK(String str, Scanner in) {
         if (str.equalsIgnoreCase("f k")) {
             System.out.println("Enter temperature(F)");
             str = in.nextLine();
             try {
                 double sum = ((Double.parseDouble(str) - 32) * 5 / 9 + 273.15D);
                 System.out.println(str + "F = " + sum + "K");
+                return sum;
             } catch (Exception e) {
                 System.out.println("Number input error. Next time, please try to enter a number. Example: 23 or 23.55");
+                return 0;
             }
         }
+        return 0;
     }
 
-    public void convertKtoC(String str, Scanner in) {
+    public double convertKtoC(String str, Scanner in) {
         if (str.equalsIgnoreCase("k c")) {
             System.out.println("Enter temperature(K)");
             str = in.nextLine();
             try {
                 double sum = (Double.parseDouble(str) - 273.15D);
                 System.out.println(str + "K = " + sum + "C");
+                return sum;
             } catch (Exception e) {
                 System.out.println("Number input error. Next time, please try to enter a number. Example: 23 or 23.55");
+                return 0;
             }
         }
+        return 0;
     }
 
-    public void convertKtoF(String str, Scanner in) {
+    public double convertKtoF(String str, Scanner in) {
         if (str.equalsIgnoreCase("k f")) {
             System.out.println("Enter temperature(K)");
             str = in.nextLine();
             try {
                 double sum = ((Double.parseDouble(str) - 273.15D) * 9 / 5 + 32);
                 System.out.println(str + "K = " + sum + "F");
+                return sum;
             } catch (Exception e) {
                 System.out.println("Number input error. Next time, please try to enter a number. Example: 23 or 23.55");
+                return 0;
             }
         }
+        return 0;
     }
 
     public void choiceConverter(String str, Scanner in) {
